be21baf17e3e2fefd7960e0389e091b5c92af37c
ArohanRudra1508 <msanchi08@gmail.com>
2024-10-16 18:56:47 +0200

    Update recycle.py

--- recycle.py
+++ recycle.py
@@ -10,8 +10,8 @@ HEIGHT = 600
 CENTREx = WIDTH/2
 CENTREy = HEIGHT/2
 CENTRE = CENTREx,CENTREy
-STARTSPEED = 5
-FINAL_LEVEL = 10
+STARTSPEED = 10
+FINAL_LEVEL = 9
 ITEMS = ["battery","bottle","chips","plasticbag"]
 
 game_over = False
@@ -20,11 +20,9 @@ current_level = 1
 items = []
 animations = []
 
-# Part 2
 def display_message(heading, subheading):
-    screen.draw.text(heading, fontsize = 60, color = "black", centre = CENTRE)
-    screen.draw.text(subheading, fontsize = 30, color = "black", centre = (350,350))
-#Part 3 
+    screen.draw.text(heading, fontsize = 60, color = "black", center = CENTRE)
+    screen.draw.text(subheading, fontsize = 30, color = "black", center = (350,350))
     
 def draw():
     global items, current_level, game_over, game_complete
@@ -36,12 +34,25 @@ def draw():
         display_message("You passed the recycling test!", "Congratulate yourself!")
     else:
         for item in items:
-            item.draw()
+          item.draw()
 
-def get_options_to_create(extra_items):
+
+def update():
+    global items
+    if len(items) == 0:
+        items = make_items(current_level)
+
+def make_items(number_of_extra_items):
+    items_to_create = get_options_to_create(number_of_extra_items)
+    new_items = create_items(items_to_create)
+    layout_items(new_items)
+    animate_items(new_items)
+    return new_items
+
+def get_options_to_create(number_of_extra_items):
     items_to_create = ["paperbag"]
-    for i in range(0,extra_items):
-        random_option = random.choice[ITEMS]
+    for i in range(number_of_extra_items):
+        random_option = random.choice(ITEMS)
         items_to_create.append(random_option)
     return items_to_create
     
@@ -52,10 +63,11 @@ def create_items(items_to_create):
         x.append(y)
     return x
 
-# Part 4
+# Part 2
 def layout_items(items_to_layout):
     number_of_gaps = len(items_to_layout) +1
     gap_size = WIDTH / number_of_gaps
+    random.shuffle(items_to_layout)
     for index,i in enumerate(items_to_layout):
         new_x_pos = (index+1) * gap_size
         i.x = new_x_pos
@@ -71,8 +83,36 @@ def animate_items(items_to_animate):
 def handle_game_over():
     global game_over
     game_over = True
-    # Part 5
 
+    # Part 3
+
+
+def on_mouse_down(pos):
+    global items, current_level
+    for item in items:
+        if item.collidepoint(pos):
+            if "paperbag" in item.image:
+                handle_game_complete()
+            else:
+                handle_game_over()
+
+def handle_game_complete():
+    global items, current_level, animations, game_complete
+    stop_animations(animations)
+    if current_level == FINAL_LEVEL:
+        game_complete = True
+    else:
+        current_level += 1
+        items = []
+        animations = []
+
+def stop_animations(animations_to_stop):
+    for animation in animations_to_stop:
+        if animation.running:
+            animation.stop()
+
+
+pgzrun.go()
 
 
 
